e2ee7c2560e10f50d05ad82d5555f663f351f0b5
codeAmyth <dev.amyth27@gmail.com>
2026-03-02 13:51:17 +0530

    fix

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  let [count, setCount] = useState(0)
+
+  const addValue = () => {
+        if (count < 20) {
+        setCount(count+1)
+        }
+  }
+
+    const decreaseValue = () => {
+     if (count > 0)  {
+        setCount(count - 1)
+     }
+      
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Divyanshu's Counter</h1>
+    <p>Count: {count}</p>
+    <button onClick={addValue}>Increase</button>
+    <br />
+    <button onClick={decreaseValue}>Decrease</button>
+
     </>
   )
 }
